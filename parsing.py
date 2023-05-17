70a9575f0b585e673be3b9c3449acf825bec2bbe
Jinhyuk Yang <jin@Jinhyukui-MacBookPro.local>
2023-05-17 09:39:15 +0900

    first commit

---
+++ parsing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from pyproj import Proj, transform
+
+# parse dataset
+df = pd.read_csv('dataset/fulldata_07_24_04_P.csv', encoding='cp949')
+
+# filter the closed/banned restaurant
+df = df[df['영업상태구분코드'] == 1]
+
+# filter the restaurant which has no position data
+df = df.dropna(subset=['좌표정보(x)', '좌표정보(y)'])
+
+# convert position data from EPSG:5174 to EPSG:4326
+proj_src = Proj(init='epsg:5174')
+proj_dst = Proj(init='epsg:4326')
+dst_positions = transform(proj_src, proj_dst, df['좌표정보(x)'].values, df['좌표정보(y)'].values)
+df['X'] = dst_positions[1]
+df['Y'] = dst_positions[0]
+
+# filter useless columns
+df = df[['사업장명', '영업상태명', 'X', 'Y', '최종수정시점']]
+
+df = df[df['사업장명'] == '조조칼국수 성수점']
+
+# look dataframe
+print(df)
